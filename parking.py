76d26550cd861b2e419ca5db261aceaf572caa5f
orimoshe <36712835+orimoshe@users.noreply.github.com>
2024-10-12 14:43:02 +0300

    Starting to make classes

---
+++ parking.py
@@ -0,0 +1,6 @@
+from property_boxes import suburbsMatrixProperties
+class ParkingClass(suburbsMatrixProperties):
+    def __init__(self):
+        suburbsMatrixProperties.__init__(self, [3,2])
+
+    
